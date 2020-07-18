932f592ef556414b1f493ef1aecd44fddbdabb0b
Glitch (activist-toolkit-aisha) <none>
2020-07-18 03:15:20 +0000

    🚙🦀 Updated with Glitch

--- script.js
+++ script.js
@@ -1,85 +1,19 @@
-//Global Variables
-//This is where you will define the variables you will be using in your project.
-//#TODO: Create four variables to track each possible quiz outcome
-var mermaidScore = 0;
-var fairyScore = 0;
-var centarScore = 0;
-var dragonScore = 0;
-var questioncount = 0;
+/************************************************/
+/********* ADD CHECKLIST FUNCTIONALITY **********/
+/************************************************/
 
-//#TODO: Use the DOM to create variables for the first quiz question.
-var q1a1 = document.getElementById("q1a1");
-var q1a2 = document.getElementById("q1a2");
-var q1a3 = document.getElementById("q1a3");
-var q1a4 = document.getElementById("q1a4");
-var q2a1 = document.getElementById("q2a1");
-var q2a2 = document.getElementById("q2a2");
-var q2a3 = document.getElementById("q2a3");
-var q2a4 = document.getElementById("q2a4");
-var q3a1 = document.getElementById("q3a1");
-var q3a2 = document.getElementById("q3a2");
-var q3a3 = document.getElementById("q3a3");
-var q3a4 = document.getElementById("q3a4");
-//#TODO: Add Event Listeners to your answer choice variables.
+/* TODO: Add a variable to store the "my-list" element */
 
-q1a1.addEventListener("click", fairy);
-q1a2.addEventListener("click", centar);
-q1a3.addEventListener("click", mermaid);
-q1a4.addEventListener("click", dragon);
-q2a1.addEventListener("click", centar);
-q2a2.addEventListener("click", fairy);
-q2a3.addEventListener("click", dragon);
-q2a4.addEventListener("click", mermaid);
-q3a1.addEventListener("click", dragon);
-q3a2.addEventListener("click", fairy);
-q3a3.addEventListener("click", centar);
-q3a4.addEventListener("click", mermaid);
+var fullItemList = document.getElementById("my-list");
 
-//#TODO: Define quiz functions here
-function fairy() {
-  fairyScore += 1;
-  questioncount += 1;
-  if (questioncount >= 3) {
-    updateresult();
+if (fullItemList) {
+  fullItemList.addEventListener("click", checkOffItem, false);
+}
+function checkOffItem(clicked) {
+  if (clicked.target.tagName == "LI") {
+    clicked.target.classList.toggle("all-done");
   }
 }
+/* TODO: Create the event listener that listens for a mouse click and runs the checkOffList function */
 
-function mermaid() {
-  mermaidScore += 1;
-  questioncount += 1;
-  if (questioncount >= 3) {
-    updateresult();
-  }
-}
-
-function dragon() {
-  dragonScore += 1;
-  questioncount += 1;
-  if (questioncount >= 3) {
-    updateresult();
-  }
-}
-
-function centar() {
-  centarScore += 1;
-  questioncount += 1;
-  if (questioncount >= 3) {
-    updateresult();
-  }
-}
-
-var result = document.getElementById("result");
-
-function updateresult() {
-  if (fairyScore >= 2) {
-    result.innerHTML = "fairy";
-  } else if (dragonScore >= 2) {
-    result.innerHTML = "dragon";
-  } else if (mermaidScore >= 2) {
-    result.innerHTML = "mermaid";
-  } else if (centarScore >= 2) {
-    result.innerHTML = "centar";
-  } else {
-    innerHTMl = "Wow! i guess you're a multi-mystical being, cool.";
-  }
-}
+/* TODO: Declare the function checkOffList and add actions inside the { } */
